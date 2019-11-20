3127274352ea52fd6f5fe7af854a69ab2a18adf4
Kendrick Gohetia <arthas777@gmail.com>
2019-11-19 21:23:25 -0500

    Player movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rBody2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Used when manipulating physics
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rBody2d.velocity = new Vector2(horizontal * speed, vertical * speed);
+    }
+}
